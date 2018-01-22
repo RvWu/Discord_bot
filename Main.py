426aefa226f9a8df76e136487bde987bd44f48fc
Richard v. W <richard@vonwurmb.de>
2018-01-22 17:23:10 +0100

    Fixed an Error in Line 30

--- Main.py
+++ Main.py
@@ -27,7 +27,7 @@ async def on_message(message):#message korrigieren
         
     if message.content.startswith('!hello'):
         msg = 'Sup {0.author.mention}'.format(message)
-await client.send_message(message.channel, msg)
+        await client.send_message(message.channel, msg)
 
 
 client.run('Token')
